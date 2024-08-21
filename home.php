2caae6275ea0ebd9f8659d21049846c3f889e457
Artur Bernardo <arturberna@gmail.com>
2024-08-21 00:44:32 -0300

    update to bootstrap 5

--- home.php
+++ home.php
@@ -9,62 +9,61 @@ if (empty($_SESSION['logged_user'])){
 <head>
   <meta charset="UTF-8">
   <title>Controle de acesso </title>
-  <link rel='stylesheet' href='https://cdn.jsdelivr.net/foundation/5.5.0/css/foundation.css'>
+  
   <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
 
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
-  <!-- <link rel="stylesheet" href="./style.css"> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="./assets/css/menu-style.css">
   
 
 </head>
 <body>
+<div class="blue-green border-bottom">
+      <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start text-large">
+          <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+          <i class="fa fa-fw fa-user"></i>
+          Logo
+          </a>
 
-<!-- Top Menu bar Navigation -->
-<nav class="top-bar" data-topbar role="navigation">
-  <ul class="title-area">
-    <li class="name">
-      <h1><a href="#">Logo</a></h1>
-    </li>
-
-    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-  </ul>
-
-  <section class="top-bar-section">
-    <ul class="right">
-        <?php 
-            if (! empty($_SESSION['pages'])){
-                foreach($_SESSION["pages"] as $page){
-                    echo '<li class="divider"></li>';
-                    echo '<li><a href="'.$page["link"].'">'.$page["nome"].'</a></li>';
-                }
-            }
-        ?>
-      
-      <li class="divider"></li>
-      <?php
-        if (! empty($_SESSION['logged_user'])){
-            echo '<li><a href="#"><i class="fa fa-fw fa-user"></i>' .$_SESSION["logged_user"].'</a></li>';
-        }
-      ?>
-      <li class="divider"></li>';
-      <li><a href="./index.php">Sair</a></li>';
-    </ul>
-  </section>
-</nav>
-
-
-<section class="hero-unit">
-
-  <div class="row">
-    <div class="large-12 columns">
-      <hgroup>
-        <h1>Insira um texto de título</h1>
-        <h4>Aqui uma pequena descrição...</h4>
-        <h3>Aqui pode ser uma imagem, ou um conjunto de cards</h3>
-      </hgroup>
+          <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+            <?php 
+              if (! empty($_SESSION['pages'])){
+                  foreach($_SESSION["pages"] as $page){
+                      echo '<li><a class="nav-link text-white" href="'.$page["link"].'">'.$page["nome"].'</a></li>';
+                  }
+              }
+            ?>
+           
+          </ul>
+          <div class="dropdown text-end">
+          <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+          </a>
+          <ul class="dropdown-menu text-small">
+            <?php
+              if (! empty($_SESSION['logged_user'])){
+                echo '<li><a class="dropdown-item" href="#">' .$_SESSION["logged_user"].'</a></li>';
+              }
+            ?>
+            
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="./index.php">Sair</a></li>
+          </ul>
+        </div>
+        </div>
+      </div>
     </div>
- </div>
-</section>
+
+
+    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Insira um texto de título</h1>
+        <p class="col-md-8 fs-4">Aqui pode ser uma imagem, um texto, ou outro tipo de componente.</p>
+        <button class="btn custom-blue btn-lg" type="button">Um botão</button>
+      </div>
+    </div>
+
 </body>
 </html>
