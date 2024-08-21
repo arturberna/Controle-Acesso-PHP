0e434794e0688af2b591226d2f051b987b7791ae
Artur Bernardo <arturberna@gmail.com>
2024-08-20 23:43:31 -0300

    Update index.php
    redireciona usuário nao logado

--- usuario/index.php
+++ usuario/index.php
@@ -1,10 +1,15 @@
+<?php
+session_start();
+if (empty($_SESSION['logged_user'])){
+    header("Location: ../index.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="utf-8">
-    <!-- Latest compiled and minified CSS -->
-    
+      
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
