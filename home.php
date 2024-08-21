35fdb7cc35555caefc48595d4f3facfe0c3387fc
Artur Bernardo <arturberna@gmail.com>
2024-08-21 09:05:42 -0300

    Update home.php

--- home.php
+++ home.php
@@ -14,7 +14,7 @@ if (empty($_SESSION['logged_user'])){
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="./assets/css/menu-style.css">
+  <link rel="stylesheet" href="./assets/css/colors.css">
   
 
 </head>
